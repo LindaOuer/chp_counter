63bd208164ef9eb6a3f317a1548bf62303beac9a
Linda Ouerfelli <linda.ouerfelli@gmail.com>
2019-06-06 15:32:20 +0100

    Updates

---
+++ precache-manifest.acb5d562eba4227fa0aaf96d7f78027b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10cfc062e6b559daac629f5c60452582",
+    "url": "/chp_counter/index.html"
+  },
+  {
+    "revision": "5681feaa87f9a998b6d2",
+    "url": "/chp_counter/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7acf6b15ec6287b870d9",
+    "url": "/chp_counter/static/js/2.9ec2803c.chunk.js"
+  },
+  {
+    "revision": "5681feaa87f9a998b6d2",
+    "url": "/chp_counter/static/js/main.a27e89dd.chunk.js"
+  },
+  {
+    "revision": "7239df3919f8afb449a7",
+    "url": "/chp_counter/static/js/runtime~main.715a1219.js"
+  }
+]);
